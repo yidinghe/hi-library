00035eda95d09615190f9a6d94aaf1a62b8e8b46
Yiding He <yhe@truxnow.com>
2020-07-01 16:22:02 -0400

    added hilog.

--- app/src/main/java/com/yhe/hi/library/app/demo/HiLogDemoActivity.kt
+++ app/src/main/java/com/yhe/hi/library/app/demo/HiLogDemoActivity.kt
@@ -3,20 +3,25 @@ package com.yhe.hi.library.app.demo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yhe.hi.library.app.R
-import com.yhe.hi.library.log.HiLog
-import com.yhe.hi.library.log.HiLogConfig
-import com.yhe.hi.library.log.HiLogType
+import com.yhe.hi.library.log.*
 import kotlinx.android.synthetic.main.activity_hi_log_demo.*
 
 class HiLogDemoActivity : AppCompatActivity() {
+
+    var viewPrinter: HiViewPrinter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hi_log_demo)
+        viewPrinter = HiViewPrinter(this)
 
         btn_log.setOnClickListener { printLog() }
+
+        viewPrinter!!.viewProvider.showFloatingView()
     }
 
     private fun printLog() {
+        HiLogManager.getInstance().addPrinter(viewPrinter)
 
         HiLog.log(object : HiLogConfig() {
             override fun includeThread(): Boolean {
