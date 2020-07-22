e3dce13052c20b3e7d8d8dca591ca00e1bbd79d4
Yiding He <yhe@truxnow.com>
2020-07-22 15:47:02 -0400

    Updaet HiDisplayUtils.

--- hilibrary/src/main/java/com/yhe/hi/library/util/HiDisplayUtil.java
+++ hilibrary/src/main/java/com/yhe/hi/library/util/HiDisplayUtil.java
@@ -14,10 +14,10 @@ public class HiDisplayUtil {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.getDisplayMetrics());
     }
 
-//    public static int dp2px(float dp) {
-//        Resources resources = AppGlobals.INSTANCE.get().getResources();
-//        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.getDisplayMetrics());
-//    }
+    public static int dp2px(float dp) {
+        Resources resources = AppGlobals.INSTANCE.get().getResources();
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.getDisplayMetrics());
+    }
 
 
     public static int getDisplayWidthInPx(@NonNull Context context) {
